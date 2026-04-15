b8af1f17ef1da52738b440ebd47a66e98c5e83dc
git-rekt7 <corealle2004@gmail.com>
2026-04-15 11:29:00 -0400

    Added 1Cee

---
+++ week-02/Ex1C_sakila_exploration.sql
@@ -0,0 +1,18 @@
+/*
+a) The columns subdirectory within "actor" contains the actor id, first name, last name, and the last update values.
+
+b) Within the columns subdirectory of "film" we see the film information (Title, Id, description release year, language)
+as well as the rental information (Rental rate, duration, replacement.)
+
+c) The "film_actor" table contains both the actor id as well as the film id.
+
+d) Clicking the select rows option creates a simple select from query for the table.
+
+e)  The inventory table includes the inventory id, film id, store id, and the date of last updates.
+
+f) We can find all films rented on a specific date using the film table and the rental table
+
+*/
+
+SELECT * FROM rental;
+SELECT * FROM inventory;
