dc9ca89492b0f3e837c34c7ac367bb616f1b3a97
git-rekt7 <corealle2004@gmail.com>
2026-04-15 10:20:34 -0400

    Added actor and film queries

--- week-02/ExB1_first_query.sql
+++ week-02/ExB1_first_query.sql
@@ -1,3 +1,7 @@
 /*
 The purpose of the sakila database is storing customer information for a movie rental store.
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+SELECT * FROM film; -- Retrieved 1000 records
