f8b86a06edad4c2bbe8dcf203d032081de9ef1af
git-rekt7 <corealle2004@gmail.com>
2026-05-04 14:56:52 -0400

    Updated ex1a

--- week-05/Hello.py
+++ week-05/Hello.py
@@ -1,1 +1,23 @@
 print ("Hello World!")
+
+message = 'Hello World!'
+print(message)
+
+# "Hello world" Prints twice because the code in line 1 and 3,
+# both call for it to be printed.
+
+
+# Displaying dollars and cents:
+
+dollars = 3
+cents = 0.50
+print(dollars + cents)
+# I notice that the result of the operation is missing a trailing zero.
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
