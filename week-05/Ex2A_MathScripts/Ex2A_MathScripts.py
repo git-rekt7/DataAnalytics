18d4625f9092f540a6a7a32d035103740217d871
git-rekt7 <corealle2004@gmail.com>
2026-05-05 16:42:01 -0400

    Committed Net Worth

---
+++ week-05/Ex2A_MathScripts/Ex2A_MathScripts.py
@@ -0,0 +1,26 @@
+# 1.
+# a) For this example, Mr Jensen owns a beautiful lake home, a puppy mill, and a 2026 Hyuandai Sonata (fancy)
+# These would be considered his assets, although the Sonata is actively depreciating.
+# Mr Jensen's debt consists of a student loans to an almost ivy league university and an average amount of credit card debt.
+
+
+# Assets
+LakeHome = 1000000
+PuppyMill = 500000
+Sonata = 39495
+
+# Debt
+StudentLoan = 38000
+CreditDebt = 6540
+
+
+# MATHEMATICS
+
+Assets = (LakeHome + PuppyMill + Sonata)
+print(f"Your total assets are: {Assets:,}")
+
+Debts = (StudentLoan + CreditDebt)
+print(f"Your total debts: {Debts:,}")
+
+NetWorth = (Assets - Debts)
+print(f"Your Net Worth is: {NetWorth:,}")
