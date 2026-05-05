f4a55a1203e16e507de3fc96acb9f12b50f39e0b
git-rekt7 <corealle2004@gmail.com>
2026-05-05 10:00:04 -0400

    1Bee

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,44 @@
+Customer_ID = '6' # numerical value represented as string to format
+Customer_name = 'JaredFogle' # name as string due to text char
+Customer_gender = 'Nonbinary' # name as string due to text char
+Customer_birth = '10/25/66' # String value due to formatting
+Driver_number = '1' # Numerical value represented as string to format
+Auto_policy = '2' # Numerical value represented as string to format
+
+MyName = 'Corey'
+City = 'Detroit'
+State = 'Michigan'
+
+
+str_1 = 'Customer ID:'
+str_2 = 'Name:'
+str_3 = 'Gender:'
+str_4 = 'Birthdate:'
+str_5 = 'Drive #:'
+str_6 = 'Policy:'
+
+print(str_1 + " " + Customer_ID)
+print(str_2 + " " + Customer_name)
+print(str_3 + " " + Customer_gender)
+print(str_4 + " " + Customer_birth)
+print(str_5 + " " + Driver_number)
+print(str_6 + " " + Auto_policy)
+
+#|      RESERVED WORDS      |#
+# A)
+# False	def	if	raise
+# None	del	import	return
+# True	elif	in	try
+# and	else	is	while
+# as	except	lambda	with
+# assert	finally	nonlocal	yield
+# break	for	not
+# class	form	or
+# continue	global	pass
+
+# B)
+#1. Import - statement used to import modules.
+#2. Else - conditional statement that tells to perform an alternate action
+#3. Is - Checks if two variables are equivalent
+#4. While - loop condition instructing to continue until a condition is no longer true
+#5.True - Boolean operator that informs the validity of a value
