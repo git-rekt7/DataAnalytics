ad4dc9eca43078abdd5f2aa39c742cb3213420b9
git-rekt7 <corealle2004@gmail.com>
2026-05-06 17:55:49 -0400

    Added Inputs to Ex2A_tips.py

---
+++ week-05/Ex2A_MathScripts/Bank_balance1.py
@@ -0,0 +1,4 @@
+# Bank Balance
+
+bank_bal = input("What is your current bank balance? ")
+print("Your balance is " + bank_bal)
---
+++ week-05/input_tip.py
@@ -0,0 +1,19 @@
+# Sample problem: How do you calculate the total due at a restaurant given the food cost,
+# tax, and tip?
+
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = float(input("How much will you tip? "))
+
+# Calculate the unknown
+total_due = food_cost + tax + tip
+
+# Display the results
+# print ("The total due is " + str(total_due))
+#                            ^^ String is needed here so that the variable can be concatenated.
+
+print("food cost is " + str(food_cost) + " and tax is " + str(tax))
+# print("Tip is " + str(tip))
+print("Tip is " + format(tip, ".2f"))
+print("Total due is " + str(total_due))
