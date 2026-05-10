6bb8c0673fef0f831c80839ac153efcc043888c6
git-rekt7 <corealle2004@gmail.com>
2026-05-10 15:23:29 -0400

    I <3 LOOPS

---
+++ week-05/Ex4C_LoopScripts/loop_love.py
@@ -0,0 +1,13 @@
+# Loop love <3
+
+# List of lovely words
+
+SweetLikeCandy = ["Fantastic", "Incredible", "Gorgeous", "Beautiful"]
+
+# Loops through the list
+
+for word in SweetLikeCandy:
+    print(f"Loops are {word}!")
+
+# I <3 loops
+print("I <3 loops")
